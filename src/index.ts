8d474007299f692501fc5c380773bf891b60b336
Abhishek Thakur <abhishek.thakur@alefeducation.com>
2024-05-08 17:35:30 +0400

    Removed circuit breaker creation, now we can pass breaker from outside

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,11 @@
 
 import client, {Gauge, Registry} from 'prom-client'
 import {
-  circuitBreaker,
   CircuitBreakerPolicy,
-  ConsecutiveBreaker,
-  ExponentialBackoff,
-  handleAll,
   IDefaultPolicyContext,
   IMergedPolicy,
   IRetryContext,
-  retry,
   RetryPolicy,
-  wrap
 } from 'cockatiel'
 
 type PrometheusMetricsOptions = {
@@ -29,34 +23,21 @@ export default class CockatielPrometheus {
   private _metricPrefix: any;
   private _client: typeof import("/Users/a.thakur/workspace/cockatiel-prometheus/node_modules/prom-client/index");
   private _options: PrometheusMetricsOptions;
-  private _circuitBreakers: {[key: string]: any};
   private _counter: Gauge;
-  private _retryPolicy: RetryPolicy;
-  private _circuitBreakerPolicy: CircuitBreakerPolicy;
   constructor (options: PrometheusMetricsOptions) {
     this._registry = client.register;
     this._metricPrefix = options.metricPrefix || ``;
     this._client = client;
     this._options = options;
-    this._circuitBreakers = {};
     this._counter = new this._client.Gauge({
       name: `${this._metricPrefix}circuit`,
       help: `A count of all circuit' events`,
       registers: [this._registry],
       labelNames: ['name', 'event', 'application']
     });
-    this._retryPolicy = retry(handleAll, { maxAttempts: options.maxAttempts || 3, backoff: new ExponentialBackoff() });
-    this._circuitBreakerPolicy = circuitBreaker(handleAll, {
-      halfOpenAfter: (options.halfOpenAfter || 2) * 1000,
-      breaker: new ConsecutiveBreaker(3),
-    });
   }
 
-  add (methodName: string): CircuitBreakerType {
-    if (!this._circuitBreakers[methodName]) {
-      this._circuitBreakers[methodName] = wrap(this._retryPolicy, this._circuitBreakerPolicy);
-    }
-    const circuitBreakerPolicy = this._circuitBreakers[methodName].wrapped[1]
+  add (methodName: string, circuitBreakerPolicy: CircuitBreakerPolicy) {
     circuitBreakerPolicy.onHalfOpen(() => {
       this._counter.labels(methodName, 'halfOpen', this._options.application).set(1)
     })
@@ -73,7 +54,6 @@ export default class CockatielPrometheus {
     circuitBreakerPolicy.onSuccess(() => {
       this._counter.labels(methodName, 'closed', this._options.application).set(1)
     })
-    return this._circuitBreakers[methodName]
   }
 
   clear () {
